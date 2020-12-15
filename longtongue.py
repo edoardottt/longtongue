90cc9a42c27bfa68bac989111b1538c7434c37ab
edoardottt <edoardott@gmail.com>
2020-12-15 21:44:42 +0100

    update

--- longtongue.py
+++ longtongue.py
@@ -1,5 +1,10 @@
 """
 
+longtongue
+
+Customized Password/Passphrase List inputting Target Info
+
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
