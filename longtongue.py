145002a6d3b9a5923e4b945d944f855564aa42ad
edoardottt <edoardott@gmail.com>
2020-12-15 21:53:37 +0100

    update

--- longtongue.py
+++ longtongue.py
@@ -1,29 +1,38 @@
-"""
-
-longtongue
-
-Customized Password/Passphrase List inputting Target Info
-
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see http://www.gnu.org/licenses/.
-
-    @Repository:    https://github.com/edoardottt/longtongue
-
-    @Author:        edoardottt, https://www.edoardoottavianelli.it
+#!/usr/bin/python3
 
 """
 
+###### longtongue ######
 
-# ----- Import external libraries -----
+    Customized Password/Passphrase List inputting Target Info
+
+    https://github.com/edoardottt/longtongue
+
+[Author]
+
+    Edoardo Ottavianelli aka edoardottt
+
+        - edoardoottavianelli.it
+        - github.com/edoardottt
+
+[License]
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+
+"""
+
+
+# ----- Import libraries -----
+
 import argparse
 import os
 import itertools
